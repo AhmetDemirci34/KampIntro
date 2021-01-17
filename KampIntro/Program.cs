00b36990dd2bdd718679ee97028b2478858d1b0a
ahmet demirci <dikimevi_ahmet@hotmail.com>
2021-01-17 21:50:56 +0300

    While Döngüsü

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,8 +6,16 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sayi = 0, toplam = 0; // değişkenlerin başlangıç değeri 0 olarak ayarlanıyor. 
+            while (sayi % 2 == 0)  // koşulda sayi değişkeninin çift olup olmadığına bakılıyor.  
+            {
+                toplam = toplam + sayi; //döngü içerisinde sayi değişkeninin değeri toplama ekleniyor.  
+                Console.Write("Bir sayı giriniz : ");
+                sayi = Convert.ToInt16(Console.ReadLine());  // Girilen değer sayi değişkenine atanıyor.  
+            }
+            Console.WriteLine("Girilen Çift Sayıların Toplamı = " + toplam); // Toplam sonuç yazdırılıyor.
 
+            Console.ReadKey();
 
         }
     }
